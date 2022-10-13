06133b3da5ff0ba3b70f2bbe51fea354a115cfdf
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2022-10-13 14:04:17 +0100

    Test Dummy - Added Collider, added script for collisions with dummy

---
+++ DebugDaddiesDungeon/Assets/TestDummy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDummy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Used for testing any collisions with the enemy
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // do whatever
+        }
+    }
+}

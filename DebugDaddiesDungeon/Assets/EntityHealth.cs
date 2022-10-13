21ef8edd2e045a96c72b97bc15e9877813674403
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2022-10-13 14:55:28 +0100

    Health System - Factory in progress

---
+++ DebugDaddiesDungeon/Assets/EntityHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityHealth : MonoBehaviour
+{
+    public GameObject entity;
+
+    private short health;
+
+    private void Start()
+    {
+        //health = entity.Get
+    }
+
+    void entityTakesDamage(short t_health, short t_damage)
+    {
+        t_health -= t_damage;
+        if (isEntityDead())
+        {
+            Destroy(entity);
+        }
+
+        //entity.health = t_health;
+    }
+
+    bool isEntityDead()
+    {
+        if (health <= 0)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ DebugDaddiesDungeon/Assets/PlayerScriptSasa.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScriptSasa : MonoBehaviour
+{
+    public short health = 100;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public short getHealth()
+    {
+        return health;
+    }
+}

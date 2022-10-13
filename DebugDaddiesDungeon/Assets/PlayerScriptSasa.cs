ff8e99fd9bb3f14a60690c501981d55a94860547
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2022-10-13 19:37:38 +0100

    Health system sorted out

--- DebugDaddiesDungeon/Assets/PlayerScriptSasa.cs
+++ DebugDaddiesDungeon/Assets/PlayerScriptSasa.cs
@@ -2,12 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// So far handles the health system of the player
+/// Has a public method that can be called to give damage to the player
+/// </summary>
+
 public class PlayerScriptSasa : MonoBehaviour
 {
-    public short health = 100;
+    public PlayerScriptSasa player;
 
-    public short getHealth()
+    public short health = 100; // Base HP set for player 
+
+
+    public void playerTakesDamage(short t_damage) // takes in the damage, takes it away from players health
     {
-        return health;
+        health -= t_damage;
+        if (isPlayerDead())
+        {
+            Destroy(player);
+        }
+    }
+
+    bool isPlayerDead() // checks if the player is dead
+    {
+        if (health <= 0)
+            return true;
+        else
+            return false;
     }
 }

6ead0fd11cfc88fe020ae89dc0d1c4791944c6e7
Adam Mcguigan <58327044+AdamMcguigan@users.noreply.github.com>
2022-10-13 14:54:39 +0100

    Started working on the HUD
    Started working on the HUD for the game, I will create the health bar first and then follow up with the Exp bar.

---
+++ DebugDaddiesDungeon/Assets/playerHUD.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHUD : MonoBehaviour
+{
+    public float health = 100; //Vid = 2;53
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

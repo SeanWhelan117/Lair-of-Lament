4f417eab0af4a1e8995df9708366d40662fc7ced
SasaKuzmanovic <janisamxdgamingstudio@gmail.com>
2022-10-20 14:47:38 +0100

    ShortRangeEnemy
    Moves left and right, when he hits the wall he rotates and continues in the opposite direction

---
+++ DebugDaddiesDungeon/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public MoveEnemy enemy;
+
+    public float speed = 2.0f;
+    private Vector2 pos;
+
+
+    private void FixedUpdate()
+    {
+        pos = enemy.transform.position;
+        enemy.transform.position = new Vector2(pos.x + speed * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("I changed my speed and rotation");
+            speed = -speed;
+            enemy.transform.eulerAngles = new Vector3(enemy.transform.eulerAngles.x, 
+                                                   enemy.transform.eulerAngles.y + 180, 
+                                                   enemy.transform.eulerAngles.z);
+        }
+    }
+}

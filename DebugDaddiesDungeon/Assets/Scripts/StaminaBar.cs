de43b1438a24c693d267cb35fbe4bf832680e87e
Adam Mcguigan <58327044+AdamMcguigan@users.noreply.github.com>
2022-10-27 19:16:55 +0100

    Creating hud - Stamina Bar & Health Bar (#25)
    * Started working on the HUD
    
    Started working on the HUD for the game, I will create the health bar first and then follow up with the Exp bar.
    
    * Added a health bar
    
    Added a health bar to the game, the color gradient changes as the player takes more damage, need to create the Exp bar still.
    
    * Started the XP Bar
    
    Started the XP bar, after that is made I can focus on getting an XP system hooked up to that after we kill some NPCs
    
    * Made a Stamina Bar
    
    Made a stamina bar and getting the internal bits to work now with the UI
    
    * Re-uploading the Stamina bar
    
    Pushing another scene with what i had done with the stamina bar as apparently the other scene cant load in now anymore. Will continue with this tomorrow
    
    * Updating Stamina Bar
    
    Made tweaks to the stamina bar it is now functioning properly when the player moves and stops moving, need to clean it up and make it blend with the health bar
    
    * Finished the Stamina Bar
    
    finished the stamina bar, going to make a start on the xp system for the game including the UI needed for that too
    
    * Additional Merge changes
    
    Just fixed some things after merging master into this branch

---
+++ DebugDaddiesDungeon/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void setStamina(float t_stamina)
+    {
+        slider.value = t_stamina;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    public void setMaxStamina(float t_stamina)
+    {
+        slider.maxValue = t_stamina;
+        slider.value = t_stamina;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+    
+
+    
+}
